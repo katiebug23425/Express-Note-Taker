6236999c5199aa0c6899840f3a92cc924681230a
Katie Bug <katierosealford@gmail.com>
2024-04-12 14:38:21 -0400

    added get requests in html routes, added to gitignore file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,15 @@
+//import what we need
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  
+router.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  
+  module.exports = router;
