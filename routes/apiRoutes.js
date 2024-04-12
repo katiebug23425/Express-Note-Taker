b2b37c99446d74b62a4427f09a52d47430dca132
Katie Bug <katierosealford@gmail.com>
2024-04-12 18:01:44 -0400

    added static for public file as well as the app.use for the route files in server.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,3 +2,4 @@ const express = require('express');
 const router = express.Router();
 const noteId = require('uuid');
 
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}!`)
