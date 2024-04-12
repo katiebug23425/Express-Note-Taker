d3584958c791ccf70073245581c72ec721e033fc
Katie Bug <katierosealford@gmail.com>
2024-04-12 19:23:20 -0400

    added api post route with input validation and error handling. changed uuid, not sure if I have it right

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+const uuid = require('uuid');
 
 router.get('/api/notes', (req, res) => {
     try {
@@ -11,6 +12,34 @@ router.get('/api/notes', (req, res) => {
         res.json(savedNotes);
     } catch (error) {
         console.error('Error reading or parsing the JSON file:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.post('/api/notes', (req, res) => {
+    try {
+        const filePath = path.join(__dirname, '../db/db.json');
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        const savedNotes = JSON.parse(fileContent);
+        let newNote;
+
+        if (req.body && req.body.title && req.body.text) {
+            newNote = {
+                title: req.body.title,
+                text: req.body.text,
+                id: uuid(),
+            };
+
+            savedNotes.push(newNote);
+
+            fs.writeFileSync(filePath, JSON.stringify(savedNotes));
+
+            res.json(savedNotes);
+        } else {
+            res.status(400).json('Note body must at least contain a title and text');
+        }
+    } catch (error) {
+        console.error('Error occurred:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
